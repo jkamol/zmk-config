e03c6ba5ddfcc1ae70c50afb4c9f55b08f89bbab
Kamol Jutiwattananukul <jkamol@gmail.com>
2025-11-28 17:10:04 +0700

    Add screenshot tap dance and fix right ctl

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 cd /workspaces/zmk/app
 west build -p -d build/left -b nice_nano_v2 -- -DSHIELD=a_dux_left -DZMK_CONFIG="../../zmk-config/config"
 west build -p -d build/right -b nice_nano_v2 -- -DSHIELD=a_dux_right -DZMK_CONFIG="../../zmk-config/config"
-# west build -d build/left -b nice_nano_v2 -- -DSHIELD=cradio_left -DZMK_CONFIG="../../zmk-config/config"
-# west build -d build/right -b nice_nano_v2 -- -DSHIELD=cradio_right -DZMK_CONFIG="../../zmk-config/config"
+# west build -p -d build/left -b nice_nano_v2 -- -DSHIELD=cradio_left -DZMK_CONFIG="../../zmk-config/config"
+# west build -p -d build/right -b nice_nano_v2 -- -DSHIELD=cradio_right -DZMK_CONFIG="../../zmk-config/config"
 cp build/left/zephyr/zmk.uf2 /workspaces/zmk-firmwares/zmk_left.uf2
 cp build/right/zephyr/zmk.uf2 /workspaces/zmk-firmwares/zmk_right.uf2
